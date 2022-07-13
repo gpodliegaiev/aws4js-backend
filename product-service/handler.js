5ca504a22395decc9aaed5eb89efc803987672a2
heorhii_podliehaiev <heorhii_podliehaiev@epam.com>
2022-07-13 22:12:39 +0300

    added prettier config and region to sls

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'
 
-module.exports.hello = async (event) => {
+module.exports.hello = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -9,10 +9,10 @@ module.exports.hello = async (event) => {
         input: event,
       },
       null,
-      2
+      2,
     ),
-  };
+  }
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+}
